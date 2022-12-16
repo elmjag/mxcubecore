7fcaf99d353718223ef37aad821b5439c8e0973d
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2022-12-16 14:24:46 +0100

    handle missing motors

--- mxcubecore/HardwareObjects/CentringMath.py
+++ mxcubecore/HardwareObjects/CentringMath.py
@@ -208,10 +208,13 @@ class CentringMath(Procedure):
         for axis in self._gonio_axes:
             if axis["type"] == "translation":
                 motname = axis["motor_name"]
-                if centrings_dictionary[motname] is not None:
-                    vector[index] = float(centrings_dictionary[axis["motor_name"]])
-                else:
-                    return None
+                try:
+                    if centrings_dictionary[motname] is not None:
+                        vector[index] = float(centrings_dictionary[axis["motor_name"]])
+                    else:
+                        return None
+                except KeyError:  # missing kappa and kappa_phi
+                    continue
                 index += 1
         return vector
 
