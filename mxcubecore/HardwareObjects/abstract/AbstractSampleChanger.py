d5fc9b7f398b691de83a5f9b0238e5edb8b1e41d
biomax service <biomax-service@b-v-biomax-web-1.maxiv.lu.se>
2023-10-23 15:50:18 +0200

    fix missing import

--- mxcubecore/HardwareObjects/abstract/AbstractSampleChanger.py
+++ mxcubecore/HardwareObjects/abstract/AbstractSampleChanger.py
@@ -135,6 +135,7 @@ from gevent import (
 
 from mxcubecore.BaseHardwareObjects import HardwareObject
 from mxcubecore.HardwareObjects.abstract.sample_changer.Container import Container
+from mxcubecore.HardwareObjects.abstract.sample_changer.Sample import Sample
 from mxcubecore.TaskUtils import task as dtask
 
 
