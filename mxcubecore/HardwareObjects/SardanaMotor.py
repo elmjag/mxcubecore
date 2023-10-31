0de5549b18142900d7962bdec979b01ec51d9d6c
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2023-10-31 15:59:04 +0100

    do not propagate inf limits

--- mxcubecore/HardwareObjects/SardanaMotor.py
+++ mxcubecore/HardwareObjects/SardanaMotor.py
@@ -1,5 +1,6 @@
 import enum
 import logging
+import math
 import time
 
 from gevent import Timeout
@@ -205,12 +206,17 @@ class SardanaMotor(AbstractMotor):
                     static_limits is returned
         """
         try:
-            self._nominal_limits = (
-                self.position_channel.info.minval,
-                self.position_channel.info.maxval,
-            )
+            _min = self.position_channel.info.minval
+            if math.isinf(self.position_channel.info.minval):
+                _min = -10000
+
+            _max = self.position_channel.info.maxval
+            if math.isinf(self.position_channel.info.maxval):
+                _max = 10000
+            self._nominal_limits = (_min, _max)
             return self._nominal_limits
-        except Exception:
+        except Exception as ex:
+            print(ex)
             return (None, None)
 
     def get_value(self):
