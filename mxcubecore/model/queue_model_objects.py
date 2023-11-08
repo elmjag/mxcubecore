178f40d7fff574960a831551f9d4b5c99f81f266
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2023-11-08 11:50:38 +0100

    dc model returns shape id if available

--- mxcubecore/model/queue_model_objects.py
+++ mxcubecore/model/queue_model_objects.py
@@ -2663,6 +2663,11 @@ def to_collect_dict(data_collection, sample, centred_pos=None):
     acq_params = acquisition.acquisition_parameters
     proc_params = data_collection.processing_parameters
 
+    try:
+        shape_id = data_collection.shape
+    except AttributeError():
+        shape_id = ""
+
     result = [
         {
             "comments": acq_params.comments,
@@ -2732,6 +2737,7 @@ def to_collect_dict(data_collection, sample, centred_pos=None):
             "motors": centred_pos.as_dict() if centred_pos is not None else {},
             "ispyb_group_data_collections": data_collection.ispyb_group_data_collections,
             "workflow_parameters": data_collection.workflow_parameters,
+            "shape": shape_id,
         }
     ]
 
