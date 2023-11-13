2c6fa6739907e4fd4b0009dba22d1a118f378139
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2023-11-13 20:54:03 +0100

    update emit signal name positionChnged -> valueChanged

--- mxcubecore/HardwareObjects/MAXIV/BioMAX/BIOMAXResolution.py
+++ mxcubecore/HardwareObjects/MAXIV/BioMAX/BIOMAXResolution.py
@@ -38,7 +38,7 @@ class BIOMAXResolution(AbstractResolution):
 
         self.update_beam_centre(self.dtox.get_value())
         self.connect(self.dtox, "stateChanged", self.dtox_state_changed)
-        self.connect(self.dtox, "positionChanged", self.dtox_position_changed)
+        self.connect(self.dtox, "valueChanged", self.dtox_position_changed)
         self.connect(self.detector, "roiChanged", self.det_roi_changed)
 
         super().init()
@@ -54,7 +54,7 @@ class BIOMAXResolution(AbstractResolution):
         self.update_detector_position()
 
     def update_detector_position(self, state=None):
-        self.emit("positionChanged", state)
+        self.emit("valueChanged", state)
 
     def det_roi_changed(self):
         self.det_width = self.detector.get_x_pixels_in_detector()
@@ -136,7 +136,6 @@ class BIOMAXResolution(AbstractResolution):
 
     def update_resolution(self, res):
         self.current_resolution = res
-        self.emit("positionChanged", (res, ))
         self.emit('valueChanged', (res, ))
 
     def res2dist(self, res=None):
