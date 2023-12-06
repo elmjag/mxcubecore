04571d0bc80ea8be6c51f03642ca1c892a3cd157
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2023-12-06 12:23:24 +0100

    display scan failed in user logger

--- mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXCollect.py
+++ mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXCollect.py
@@ -272,6 +272,7 @@ class MICROMAXCollect(AbstractCollect, HardwareObject):
 
         except Exception as ex:
             self.log.error("[COLLECT] Data collection failed: %s" % ex)
+            self.user_log.error("[COLLECT] Data collection failed: %s" % ex)
             self.emit_collection_failed()
             self.close_fast_shutter()
             self.close_detector_cover()
@@ -1263,7 +1264,7 @@ class MICROMAXCollect(AbstractCollect, HardwareObject):
         """
         try:
             chan = self.getChannelObject("undulator_gap")
-            gap = "{:.2f}".format(chan.getValue())
+            gap = "{:.2f}".format(chan.get_value())
             return gap
         except Exception:
             return None
