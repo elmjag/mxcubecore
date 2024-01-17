a25482222c1f56caf073e14ddf155d89522b8e7f
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2024-01-17 20:46:37 +0100

    blacking

--- mxcubecore/HardwareObjects/MAXIV/BioMAX/BIOMAXResolution.py
+++ mxcubecore/HardwareObjects/MAXIV/BioMAX/BIOMAXResolution.py
@@ -6,7 +6,6 @@ import math
 from scipy.constants import kilo, h, c, eV, angstrom
 
 
-
 class BIOMAXResolution(AbstractResolution):
     def __init__(self, *args, **kwargs):
         AbstractResolution.__init__(self, *args, **kwargs)
@@ -43,7 +42,6 @@ class BIOMAXResolution(AbstractResolution):
     def update_detector_state(self, state=None):
         self.emit("stateChanged", state)
 
-
     def dtox_position_changed(self, state=None):
         self.update_detector_position()
 
@@ -58,17 +56,20 @@ class BIOMAXResolution(AbstractResolution):
 
     def update_beam_centre(self, dtox):
         beam_x, beam_y = self.get_beam_centre(dtox)
-        self.det_radius =  min(self.det_width - beam_x, self.det_height - beam_y, beam_x, beam_y)*0.075
+        self.det_radius = (
+            min(self.det_width - beam_x, self.det_height - beam_y, beam_x, beam_y)
+            * 0.075
+        )
 
     def get_beam_centre(self, dtox=None):
         if dtox is None:
             dtox = self.dtox.get_value()
-        ax = float(self.detector['beam'].get_property('ax'))
-        bx = float(self.detector['beam'].get_property('bx'))
-        ay = float(self.detector['beam'].get_property('ay'))
-        by = float(self.detector['beam'].get_property('by'))
+        ax = float(self.detector["beam"].get_property("ax"))
+        bx = float(self.detector["beam"].get_property("bx"))
+        ay = float(self.detector["beam"].get_property("ay"))
+        by = float(self.detector["beam"].get_property("by"))
 
-        return float(dtox)*ax+bx, float(dtox)*ay+by
+        return float(dtox) * ax + bx, float(dtox) * ay + by
 
     def recalculate_resolution(self):
         self.current_resolution = self.dist2res(self.dtox.get_value())
@@ -97,7 +98,6 @@ class BIOMAXResolution(AbstractResolution):
             self.dist2res(_high),
         )
 
-
     def _set_value(self, value):
         """Move resolution to value.
         Args:
@@ -115,7 +115,7 @@ class BIOMAXResolution(AbstractResolution):
             ttheta = math.atan(radius / dist)
 
             if ttheta != 0:
-                return current_wavelength / (2*math.sin(ttheta/2))
+                return current_wavelength / (2 * math.sin(ttheta / 2))
             else:
                 return None
         except Exception:
@@ -130,7 +130,7 @@ class BIOMAXResolution(AbstractResolution):
 
     def update_resolution(self, res):
         self.current_resolution = res
-        self.emit('valueChanged', (res, ))
+        self.emit("valueChanged", (res,))
 
     def res2dist(self, res=None):
         current_wavelength = self.get_wavelength()
