7661c6fe214b57ee785b892f8344fb64be0d35f0
biomax service <biomax-service@b-v-biomax-web-1.maxiv.lu.se>
2024-03-13 11:34:23 +0100

    fix shutter close method

--- mxcubecore/HardwareObjects/MAXIV/BioMAX/BIOMAXCollect.py
+++ mxcubecore/HardwareObjects/MAXIV/BioMAX/BIOMAXCollect.py
@@ -1012,7 +1012,7 @@ class BIOMAXCollect(DataCollect):
         """
         try:
             logging.getLogger("HWR").info("Closing the detector cover")
-            self.detector_cover_hwobj.closeShutter()
+            self.detector_cover_hwobj.close()
         except:
             logging.getLogger("HWR").exception("Could not close the detector cover")
 
