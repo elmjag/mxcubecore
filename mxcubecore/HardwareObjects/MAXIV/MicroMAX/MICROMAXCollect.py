e11a116152bb1a1ce82283557e296f8dc240d60e
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-05-20 12:05:33 +0200

    MicroMAX: move MD3 to center position after data collection
    When we are done with a data collection sequnce, move MD3 to
    center position.

--- mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXCollect.py
+++ mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXCollect.py
@@ -1300,6 +1300,7 @@ class MICROMAXCollect(DataCollect):
         self.diffractometer_hwobj.abort()
         self.detector_hwobj.abort()
         self.detector_hwobj.disarm()
+        self.move_to_centered_position()
 
         try:
             self.progress_task.kill(block=False)
