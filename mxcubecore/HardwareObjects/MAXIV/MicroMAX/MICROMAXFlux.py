924fb3216b0891d59774030bf61a0085bab9730b
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-06-04 12:12:48 +0200

    MicroMAX flux: fix formatting
    Follow black formatting rules.

--- mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXFlux.py
+++ mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXFlux.py
@@ -5,34 +5,51 @@ import tango
 
 
 class MICROMAXFlux(AbstractFlux):
-
     def __init__(self, name):
 
         AbstractFlux.__init__(self, name)
 
-        self.al_coeff = [2.70033918627e-07, -2.98687890398e-05,
-                         0.00123604226164, -0.0258445478508,
-                         0.443366527092, -1.88717819273,
-                         6.51896436249, -8.15712686311]
+        self.al_coeff = [
+            2.70033918627e-07,
+            -2.98687890398e-05,
+            0.00123604226164,
+            -0.0258445478508,
+            0.443366527092,
+            -1.88717819273,
+            6.51896436249,
+            -8.15712686311,
+        ]
 
-        self.sipha_coeff = [5.6946e-08,  -7.20953e-06,
-                            0.000359681, -0.00844784,
-                            0.242434, -0.960961,
-                            4.52577, -7.00435]
+        self.sipha_coeff = [
+            5.6946e-08,
+            -7.20953e-06,
+            0.000359681,
+            -0.00844784,
+            0.242434,
+            -0.960961,
+            4.52577,
+            -7.00435,
+        ]
 
-        self.air_coeff = [-4.93236e-03,  6.84953e-01,
-                          -35.9648, 845.516,
-                          -8440.87, 66209.7,
-                          -225579, 309746]
+        self.air_coeff = [
+            -4.93236e-03,
+            6.84953e-01,
+            -35.9648,
+            845.516,
+            -8440.87,
+            66209.7,
+            -225579,
+            309746,
+        ]
 
         self.al_model = np.array(self.al_coeff)
         self.sipha_model = np.array(self.sipha_coeff)
         self.air_model = np.array(self.air_coeff)
 
         self.e = 1.6022e-16
-        self.al_thickness = 1632 # um
+        self.al_thickness = 1632  # um
         self.airsample_length = 0
-        self.detdiode_length = 45.
+        self.detdiode_length = 45.0
 
         self.detector_hwobj = None
         self.detector_distance_hwobj = None
@@ -54,7 +71,7 @@ class MICROMAXFlux(AbstractFlux):
         self.det_mot = {}
 
     def init(self):
-        self.energy_hwobj = self.get_object_by_role('energy')
+        self.energy_hwobj = self.get_object_by_role("energy")
         self.detector_hwobj = HWR.beamline.detector
         self.opt_diode["ch1"] = tango.DeviceProxy("expchan/albaem_ctrl_02/2")
         self.opt_diode["ch2"] = tango.DeviceProxy("expchan/albaem_ctrl_02/3")
@@ -69,20 +86,32 @@ class MICROMAXFlux(AbstractFlux):
         return self.current_flux
 
     def transmit(self, length, energy, model):
-        att_l = model[0] * energy**7 + model[1] * energy**6 \
-            + model[2] * energy**5 + model[3] * energy**4 \
-            + model[4] * energy**3 + model[5] * energy**2 \
-            + model[6] * energy + model[7]
+        att_l = (
+            model[0] * energy**7
+            + model[1] * energy**6
+            + model[2] * energy**5
+            + model[3] * energy**4
+            + model[4] * energy**3
+            + model[5] * energy**2
+            + model[6] * energy
+            + model[7]
+        )
         transmission = np.exp(-length / att_l)
         return transmission
 
     def _calc_flux(self, energy, dist, current):
-        spectra_resp_factor = (0.1487-0.1896)/(14400-12500)
+        spectra_resp_factor = (0.1487 - 0.1896) / (14400 - 12500)
         spectra_resp = (energy - 12500) * spectra_resp_factor + 0.1896
         flux = current / spectra_resp / (1.6e-19 * energy)
         air_transmission = self.transmit(dist * 1000.0, energy / 1000.0, self.air_coeff)
-        al_transmission = self.transmit(self.al_thickness, energy / 1000.0, self.al_coeff)
-        print("Air transmission is {} al_transmission is {}".format(air_transmission, al_transmission))
+        al_transmission = self.transmit(
+            self.al_thickness, energy / 1000.0, self.al_coeff
+        )
+        print(
+            "Air transmission is {} al_transmission is {}".format(
+                air_transmission, al_transmission
+            )
+        )
         full_flux = flux / air_transmission
         return full_flux
 
@@ -100,19 +129,26 @@ class MICROMAXFlux(AbstractFlux):
         return flux
 
     def check_beam_opt(self):
-        energy_ev = self.energy_hwobj.get_current_energy() *1000.0
+        energy_ev = self.energy_hwobj.get_current_energy() * 1000.0
         msg = ""
         total = 0.0
-        for i in range(1,5):
+        for i in range(1, 5):
             ch_id = "ch{}".format(i)
             current = self.opt_diode[ch_id].InstantCurrent
             msg += " {} {},".format(ch_id, current)
-            total+= current
-        flux = total * ( -0.534515 * energy_ev * energy_ev * energy_ev * energy_ev - \
-               43197.6 * energy_ev * energy_ev * energy_ev + 5.13449e+09 * energy_ev * energy_ev \
-               - 4.39169e+13 * energy_ev + 1.14591e+17)
-        final_msg = "The currents on b312a-o06-ctl-aemhv-01 are {} sum value is {}".format(msg, total)
+            total += current
+        flux = total * (
+            -0.534515 * energy_ev * energy_ev * energy_ev * energy_ev
+            - 43197.6 * energy_ev * energy_ev * energy_ev
+            + 5.13449e09 * energy_ev * energy_ev
+            - 4.39169e13 * energy_ev
+            + 1.14591e17
+        )
+        final_msg = (
+            "The currents on b312a-o06-ctl-aemhv-01 are {} sum value is {}".format(
+                msg, total
+            )
+        )
         print(final_msg)
         print("Estimated flux is {:.2e} ph/s".format(flux))
         return final_msg, flux
-
