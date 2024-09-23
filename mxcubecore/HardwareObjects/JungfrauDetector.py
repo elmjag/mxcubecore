05c2c25f88616b69b9390200577ae6720bb4f968
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-09-23 16:27:22 +0200

    Jungfrau: make 'images per file' configurable
    Adds an optional 'images_per_file' configuration property to the
    Jungfrau hardware object. This allows to configure how many images
    Jungfrau writes to each HDF5 file during data collection.
    
    Set the default value to 1000 images per file.

--- mxcubecore/HardwareObjects/JungfrauDetector.py
+++ mxcubecore/HardwareObjects/JungfrauDetector.py
@@ -24,6 +24,9 @@ from mxcubecore.utils.units import (
     us_to_sec,
 )
 
+# default value for 'images_per_file' config property
+DEFAULT_IMAGES_PER_FILE = 1000
+
 # custom tango reply timeout
 TANGO_TIMEOUT_MS = 6000
 
@@ -51,6 +54,8 @@ class JungfrauDetector(AbstractDetector):
 
     Hardware object properties:
         detector_device (str): name of the detector's tango device
+        images_per_file (int): number of images in a single HDF5 data file,
+                               if 0 then all images go into to a single data file
     """
 
     # pixel size in millimeters (75 µm)
@@ -83,6 +88,13 @@ class JungfrauDetector(AbstractDetector):
 
     def init(self):
         super().init()
+
+        # read the optional 'images_per_file' config property
+        self.col_config["ImagesPerFile"] = self.get_property(
+            "images_per_file",
+            DEFAULT_IMAGES_PER_FILE,
+        )
+
         self.dev = DeviceProxy(self.detector_device)
         # Arm() command can block for more than 3 seconds,
         # increase the default tango timeout
@@ -180,6 +192,7 @@ class JungfrauDetector(AbstractDetector):
         dev.detector_distance_mm = meter_to_mm(config["DetectorDistance"])
         dev.images_per_trigger = config["NbImages"]
         dev.ntrigger = config["NbTriggers"]
+        dev.images_per_file = config["ImagesPerFile"]
 
         maybe_set("unit_cell__a", "UnitCellA")
         maybe_set("unit_cell__b", "UnitCellB")
