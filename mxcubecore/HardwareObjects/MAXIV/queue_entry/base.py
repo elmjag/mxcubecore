c0586178463148c4002aa1c775f63baf417e4a31
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-12-10 08:58:36 +0100

    SSX Injector: configure EIGER's trigger mode correctly
    EIGER needs to be explicitly put into either 'software triggered' mode,
    aka 'ints' or 'externally triggered' mode aka 'exts'.
    
    Make sure we configure correct mode, depending on which SSX Injector
    task we are running.

--- mxcubecore/HardwareObjects/MAXIV/queue_entry/base.py
+++ mxcubecore/HardwareObjects/MAXIV/queue_entry/base.py
@@ -53,7 +53,7 @@ class AbstractSsxQueueEntry(BaseQueueEntry):
         queue_model = HWR.beamline.queue_model
         path_template.run_number = queue_model.get_next_run_number(path_template)
 
-    def prepare_data_collection(self, num_images, num_triggers):
+    def prepare_data_collection(self, num_images, num_triggers, software_trigger=False):  # noqa: FBT002, PLR0915
         """
         Prepare beamline for a SSX data collection, i.e.:
 
@@ -160,6 +160,10 @@ class AbstractSsxQueueEntry(BaseQueueEntry):
             det_cfg["UnitCellAlpha"] = cell_alpha
             det_cfg["UnitCellBeta"] = cell_beta
             det_cfg["UnitCellGamma"] = cell_gamma
+        else:
+            # Eiger's trigger mode must be explicitly configured each time
+            trigger_mode = "ints" if software_trigger else "exts"
+            det_cfg["TriggerMode"] = trigger_mode
 
         detector.prepare_acquisition(det_cfg)
         detector.wait_config_done()
