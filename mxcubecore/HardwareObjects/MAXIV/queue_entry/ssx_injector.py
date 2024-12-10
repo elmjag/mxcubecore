255667058d9fff980c2335c30c4eefdc655a8925
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-12-10 08:58:36 +0100

    SSX Injector: configure EIGER's trigger mode correctly
    EIGER needs to be explicitly put into either 'software triggered' mode,
    aka 'ints' or 'externally triggered' mode aka 'exts'.
    
    Make sure we configure correct mode, depending on which SSX Injector
    task we are running.

--- mxcubecore/HardwareObjects/MAXIV/queue_entry/ssx_injector.py
+++ mxcubecore/HardwareObjects/MAXIV/queue_entry/ssx_injector.py
@@ -106,7 +106,9 @@ class SsxInjectorQueueEntry(AbstractSsxQueueEntry):
     def _do_data_collection(self):
         params = self._data_model._task_data.user_collection_parameters
 
-        self.prepare_data_collection(params.num_images, num_triggers=1)
+        self.prepare_data_collection(
+            params.num_images, num_triggers=1, software_trigger=True
+        )
 
         detector = HWR.beamline.detector
         log.info("Sending software trigger to detector.")
