1b5cb03c4d8dd1d5df468a085eb555077e253207
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2025-01-16 12:01:25 +0100

    ISPyB LIMS: move get_lims_name() to parent class
    Identical implementation of get_lims_name() method can be used by
    both ProposalTypeISPyBLims and UserTypeISPyBLims classes.
    
    Move this method to the parent class, to make it available for
    UserTypeISPyBLims class as well.

--- mxcubecore/HardwareObjects/abstract/ISPyBAbstractLims.py
+++ mxcubecore/HardwareObjects/abstract/ISPyBAbstractLims.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import logging
 import warnings
+from typing import List
 
 import gevent
 
@@ -9,6 +10,7 @@ from mxcubecore import HardwareRepository as HWR
 from mxcubecore.HardwareObjects.abstract.AbstractLims import AbstractLims
 from mxcubecore.HardwareObjects.abstract.ISPyBDataAdapter import ISPyBDataAdapter
 from mxcubecore.model.lims_session import LimsSessionManager
+from mxcubecore.model.lims_session import Lims
 
 
 class ISPyBAbstractLIMS(AbstractLims):
@@ -84,6 +86,14 @@ class ISPyBAbstractLIMS(AbstractLims):
                     self._translations[code]["gui"] = proposal.gui
                 except AttributeError:
                     pass
+
+    def get_lims_name(self) -> List[Lims]:
+        return [
+            Lims(
+                name="ISPyB",
+                description="Information System for protein Crystallographic Beamlines",
+            )
+        ]
 
     def get_user_name(self):
         raise Exception("Abstract class. Not implemented")
