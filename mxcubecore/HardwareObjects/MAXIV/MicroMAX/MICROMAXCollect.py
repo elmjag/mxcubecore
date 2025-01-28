638a87de0f3b3f18cfc5175d56a83587d844ae8a
Dominika Trojanowska <dominika.trojanowska@s2innovation.com>
2025-01-28 17:00:36 +0100

    MicroMAX: emit collection failed signal
    If we fail to move detector during data collection, emit collection
    failed signal. This will mark the task as failed in the queue.
    
    If we don't send this signal, the task will be stuck in
    'progressing' mode until user stops the queue.

--- mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXCollect.py
+++ mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXCollect.py
@@ -1124,6 +1124,7 @@ class MICROMAXCollect(DataCollect):
                     self.user_log.error("Cannot move detector.")
                     self.log.exception("Problems when moving detector!!")
                     self.stop_collect()
+                    self.emit_collection_failed()
         else:
             self.log.exception("Can't get distance limits, not moving detector!!")
         self.log.info(
