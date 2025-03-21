5cfd3e4bd3625635b81e8207a1d98254db107371
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2025-03-21 12:05:43 +0100

    issue MD3 abort command first thing when aborting tasks
    When aborting a data collection task, start with sending 'abort'
    command to MD3. This way we close the fast shutter as soon as
    possible. This makes it more likely that we will save the sample on
    task abort.

--- mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXCollect.py
+++ mxcubecore/HardwareObjects/MAXIV/MicroMAX/MICROMAXCollect.py
@@ -1260,8 +1260,8 @@ class MICROMAXCollect(DataCollect):
         Stops data collection
         """
         self.log.warning("Stopping collection ....")
-        self.close_detector_cover()
         self.diffractometer_hwobj.abort()
+        self.close_detector_cover()
         self.detector_hwobj.abort()
         self.detector_hwobj.disarm()
         self.move_to_centered_position()
