8cb0ae06534705fcb585523fb89aba34a4e35f10
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2025-09-04 11:28:26 +0200

    MAXIV beamline HWO: remove unneed overrides
    The local implementation of `__init__()` and `init()` does not
    perform any custom actions. We can use the superclass's
    implementation as-is.

--- mxcubecore/HardwareObjects/MAXIV/MAXIVBeamline.py
+++ mxcubecore/HardwareObjects/MAXIV/MAXIVBeamline.py
@@ -9,12 +9,6 @@ from mxcubecore.HardwareObjects.Beamline import Beamline
 
 
 class MAXIVBeamline(Beamline):
-    def __init__(self, name):
-        super(MAXIVBeamline, self).__init__(name)
-
-    def init(self):
-        super(MAXIVBeamline, self).init()
-
     def emulate(self, feature: str) -> bool:
         """Check if some feature should be emulated."""
         emulate = self.get_property("emulate", {})
