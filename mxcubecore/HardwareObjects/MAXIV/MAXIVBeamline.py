7ef8c49194af65bd58c3c7e42e30317434184f1d
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2025-09-04 15:09:26 +0200

    MAXIV beamline HWO: expand documentation

--- mxcubecore/HardwareObjects/MAXIV/MAXIVBeamline.py
+++ mxcubecore/HardwareObjects/MAXIV/MAXIVBeamline.py
@@ -1,6 +1,21 @@
-"""
-MAXIV Beamline hardware object.
+"""Custom MAXIV Beamline object.
 
+Adds support for ``emulate`` configurable, which allows to specify
+features to be emulated at the beamline.
+
+Emulation of following features are supported:
+
+* safety_shutter - emulate safety shutter opening
+* detector_cover - emulate detector cover opening
+* detector_motion - emulate detector distance moves
+
+Example of ``emulate`` configuration::
+
+  configuration:
+    emulate:
+      safety_shutter: true
+      detector_cover: true
+      detector_motion: true
 """
 
 # ruff: noqa: N999
